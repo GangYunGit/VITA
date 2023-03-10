76fe761b13598f07f4021f182d61e4271bc62ba9
김아영 (KIM A YOUNG) <joen00@naver.com>
2023-03-10 13:20:37 +0900

    [feat] : add wearable entity

---
+++ back/vita-wearable/src/main/java/com/ssafy/vitawearable/Entity/TotalScore.java
@@ -0,0 +1,35 @@
+package com.ssafy.vitawearable.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "total_score")
+public class TotalScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long totalScoreId;
+
+    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Users.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    private int totalScoreWeight;
+    private int totalScoreEnergy;
+    private int totalScoreRhr;
+    private int totalScoreStress;
+    private int totalScoreStep;
+    private int totalScoreSleep;
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private ZonedDateTime createdDate;
+
+}
---
+++ back/vita-wearable/src/main/java/com/ssafy/vitawearable/Entity/Users.java
@@ -0,0 +1,39 @@
+package com.ssafy.vitawearable.Entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class Users {
+
+    @Id
+    private String userId;
+    private String userImg;
+    private String userNickname;
+    private int userAge;
+    private String userSex;
+    private int userHeight;
+    private int userWeight;
+    private int userScore;
+    private String userPhoneType;
+    private Boolean userPublic;
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private ZonedDateTime createdDate;
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private ZonedDateTime updatedDate;
+
+}
