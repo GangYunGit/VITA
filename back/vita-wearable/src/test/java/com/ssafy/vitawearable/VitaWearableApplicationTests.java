43862797a31c78ebb3fd073612c244b5fe046b61
김아영 (KIM A YOUNG) <joen00@naver.com>
2023-03-10 12:10:38 +0900

    [feat] : add wearable project

---
+++ back/vita-wearable/src/test/java/com/ssafy/vitawearable/VitaWearableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.vitawearable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaWearableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
