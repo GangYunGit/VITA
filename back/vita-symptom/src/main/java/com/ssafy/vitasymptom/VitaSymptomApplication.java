545b6c3549c550977f839a09e1fedb51858563dc
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-03-10 12:26:21 +0900

    [feat] : add symptom project

---
+++ back/vita-symptom/src/main/java/com/ssafy/vitasymptom/VitaSymptomApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.vitasymptom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaSymptomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaSymptomApplication.class, args);
+	}
+
+}
---
+++ back/vita-symptom/src/test/java/com/ssafy/vitasymptom/VitaSymptomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.vitasymptom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaSymptomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
