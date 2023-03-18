acb0e400f1a362e3cd14643c0476d873de061cf7
차현경 <h30young@naver.com>
2023-03-18 18:08:30 +0900

    [feat] : add bigdata project

---
+++ back/vita-bigdata/src/main/java/com/ssafy/vitabigdata/VitaBigdataApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.vitabigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaBigdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaBigdataApplication.class, args);
+	}
+
+}
---
+++ back/vita-bigdata/src/test/java/com/ssafy/vitabigdata/VitaBigdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.vitabigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaBigdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
