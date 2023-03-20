9bf309049406c882a8c7a755c25b91feb889c9c6
김아영 <joen00@naver.com>
2023-03-21 01:02:28 +0900

    [feat] : front friend comp

--- front/vita-front/src/views/friend/FriendListView.vue
+++ front/vita-front/src/views/friend/FriendListView.vue
@@ -1,58 +1,140 @@
 <template>
- <div class="text-center">
+  <div class="text-center">
     <div class="container">
-        <div id="header">
-          <div id="header-title">프렌즈</div>
-          <div id="header-content">친구들의 정보를 확인해보세요.</div>
+      <div id="header">
+        <div id="header-title">프렌즈</div>
+        <div id="header-content">친구들의 정보를 확인해보세요.</div>
+      </div>
+      <div id="middle">
+        <b-button v-b-modal.modal-scrollable id="btn-add-friend"
+          >친구 추가</b-button
+        >
+        <!-- 모달창 -->
+        <friend-add-modal></friend-add-modal>
+        <!-- 모달창 -->
+        <b-form-input
+          id="nick-search"
+          placeholder="닉네임을 입력하세요."
+        ></b-form-input>
+        <button>검색</button>
+      </div>
+      <div id="list">
+        <h3 id="list-h3">친구리스트</h3>
+        <div id="list-add-post">
+          <div
+            id="list-post-div"
+            v-for="lists in friendpostlist"
+            :key="lists.id"
+          >
+            <span style="font-weight: 600">
+              {{ lists.id }}
+            </span>
+            <b-avatar
+              variant="info"
+              src="https://placekitten.com/300/300"
+            ></b-avatar>
+            <span style="font-weight: 600">{{ lists.name }} </span>
+            <button id="btn-post-add">수락</button>
+            <button id="btn-post-delete">거절</button>
+          </div>
         </div>
-            <div>
-                <b-button style="background: #B9C6F4; border: none; color: black; font-weight: 600;" to="/friendlist">친구 추가</b-button>
-                <div id="nick-search">닉네임을 입력하세요.</div>
-            </div>
-            <div id="rank-list">
-              <h3 id="list-h3">친구리스트</h3>
-            </div>
-    </div>
-    </div>
 
+        <div id="list-div" v-for="lists in friendlist" :key="lists.id">
+          <span style="font-weight: 600">
+            {{ lists.id }}
+            <b-avatar
+              variant="info"
+              src="https://placekitten.com/300/300"
+            ></b-avatar
+          ></span>
+          <span style="font-weight: 600">{{ lists.name }} </span>
+          <button id="btn-delete-friend">친구 삭제</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import FriendAddModal from "@/components/friend/FriendAddModal.vue";
 export default {
-
-}
+  name: "FriendListView",
+  components: {
+    FriendAddModal,
+  },
+  data: () => ({
+    friendlist: [
+      { id: 1, name: "김광배", score: "10" },
+      { id: 2, name: "이광배", score: "20" },
+      { id: 3, name: "차광배", score: "30" },
+    ],
+    friendpostlist: [
+      { id: 1, name: "김광배", score: "10" },
+      { id: 2, name: "이광배", score: "20" },
+    ],
+  }),
+};
 </script>
 
 <style>
-
 /* ----------------------------------------- */
 #header {
   width: 100%;
   height: 16%;
   margin-bottom: 2rem;
 }
-#header-title{
-  font-family: 'Inter';
+#header-title {
+  font-family: "Inter";
   font-style: normal;
   font-weight: 600;
   font-size: 3rem;
   line-height: 97px;
   color: #172176;
 }
-#header-content{
-  font-family: 'Inter';
+#header-content {
+  font-family: "Inter";
   font-style: normal;
   font-weight: 600;
   font-size: 1rem;
   line-height: 24px;
   text-align: center;
   margin: -1%;
-  color: #47474B;
+  color: #47474b;
 }
-#rank-list {
+#middle {
+  height: 4rem;
+  margin: 0 auto;
+  vertical-align: middle;
+}
+#btn-add-friend {
+  width: 10%;
+  height: 40px;
+  border: none;
+  color: rgb(255, 255, 255);
+  font-weight: 600;
+  background: #3695be;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  margin-left: 26rem;
+  margin-right: 1rem;
+  float: left;
+}
+#nick-search {
+  box-sizing: border-box;
+  width: 25%;
+  height: 40px;
+  font-weight: 600;
+  color: #3695be;
+  border: 2px solid #3695be;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 15px;
+  float: left;
+  padding-top: 0.3rem;
+}
+#list {
   padding: 0.5rem;
   width: 40%;
-  height: 25rem;
+  height: 27rem;
   background-color: aquamarine;
   margin: 0 auto;
   margin-top: 0.5rem;
@@ -60,21 +142,66 @@ export default {
   background: rgba(255, 255, 255, 0.8);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
+  clear: both;
 }
-#list-h3{
-  font-family: 'Exo 2';
+#list-h3 {
+  font-family: "Exo 2";
   font-style: normal;
   font-weight: 700;
   font-size: 26px;
   color: #172176;
 }
-#nick-search{
-    box-sizing: border-box;
-    width: 200px;
-    height: 40px;
+#list-div {
+  width: 90%;
+  height: 3.5rem;
+  background: #e0f4fd;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  margin: 1rem;
+  padding-top: 0.9rem;
+}
+#btn-delete-friend {
+  width: 89px;
+  height: 24px;
+  font-weight: 600;
+  color: white;
+  background: #ffa755;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  border: none;
+}
 
-    border: 2px solid #3695BE;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    border-radius: 20px;
+#btn-post-add {
+  width: 55px;
+  height: 24px;
+
+  background: #79b6fd;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  color: white;
+  font-weight: 600;
+  border: none;
+  margin-right: 1rem;
+}
+#btn-post-delete {
+  width: 55px;
+  height: 24px;
+  font-weight: 600;
+  color: white;
+  background: #ffa755;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  border: none;
+}
+
+#list-post-div {
+  width: 90%;
+  height: 3.5rem;
+  background: #3695be;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  margin: 1rem;
+  color: white;
+  padding-top: 0.9rem;
 }
 </style>
--- front/vita-front/src/views/friend/FriendView.vue
+++ front/vita-front/src/views/friend/FriendView.vue
@@ -1,62 +1,85 @@
 <template>
-   <div class="text-center">
+  <div class="text-center">
     <div class="container">
-        <div class="header">
-          <div class="header-title">랭킹</div>
-          <div class="header-content">친구들의 순위를 확인해보세요.</div>
-        </div>
-        <div class="left">
-            <b-button style="background: #B9C6F4; border: none; color: black; font-weight: 600;" to="/friendlist">친구 리스트 보러가기</b-button>
-            <div class="rank-list">
-              <h3 class="rank-list-h3">종합점수</h3>
-              <div class="rank-list-div" v-for="totalrank in Totalranks" :key="totalrank.id">
-
-                 <span> {{ totalrank.id }}  <b-avatar src=""></b-avatar></span>
-                <span>{{ totalrank.name }} </span>
-                <span>{{ totalrank.score }} </span>
-              </div>
-            </div>
-        </div>
-        <div class="right">
-          <p style="font-weight: 700; font-size: 26px;   color: #172176;">각 종합점수</p>
-          <div class="scroll">
-            <friend-walk></friend-walk>
-            <friend-walk></friend-walk>
-            <friend-walk></friend-walk>
-            <friend-walk></friend-walk>
-            <friend-walk></friend-walk>
+      <div class="header">
+        <div class="header-title">랭킹</div>
+        <div class="header-content">친구들의 순위를 확인해보세요.</div>
+      </div>
+      <div class="left">
+        <b-button
+          style="
+            background: #b9c6f4;
+            border: none;
+            color: black;
+            font-weight: 600;
+          "
+          to="/friendlist"
+          >친구 리스트 보러가기</b-button
+        >
+        <div class="rank-list">
+          <h3 class="rank-list-h3">종합점수</h3>
+          <div
+            class="rank-list-div"
+            v-for="totalrank in Totalranks"
+            :key="totalrank.id"
+          >
+            <span> {{ totalrank.id }} <b-avatar src=""></b-avatar></span>
+            <span>{{ totalrank.name }} </span>
+            <span>{{ totalrank.score }} </span>
           </div>
         </div>
       </div>
+      <div class="right">
+        <p style="font-weight: 700; font-size: 26px; color: #172176">
+          각 종합점수
+        </p>
+        <div class="scroll">
+          <friend-walk></friend-walk>
+          <friend-walk></friend-walk>
+          <friend-walk></friend-walk>
+          <friend-walk></friend-walk>
+          <friend-walk></friend-walk>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import FriendWalk from "@/components/friend/FriendWalk.vue"
+import FriendWalk from "@/components/friend/FriendWalk.vue";
 export default {
-  name: 'FriendView',
+  name: "FriendView",
   components: {
     FriendWalk,
   },
   data: () => ({
-    Totalranks: [  { id: 1, name: '김광배', score:'10' },
-      { id: 2, name: '이광배', score:'20' },
-      { id: 3, name: '차광배', score: '30' },
-      { id: 3, name: '차광배', score: '30' },
-      { id: 3, name: '차광배', score: '30' },],
+    Totalranks: [
+      { id: 1, name: "김광배", score: "10" },
+      { id: 2, name: "이광배", score: "20" },
+      { id: 3, name: "차광배", score: "30" },
+      { id: 3, name: "차광배", score: "30" },
+      { id: 3, name: "차광배", score: "30" },
+    ],
   }),
-}
-
-
-
+};
 </script>
 
 <style>
-
 /* ----------------------------------------- */
 .text-center {
   /* background-image: url("../assets/background.png"); */
-  background: linear-gradient(146.55deg, rgba(174, 162, 220, 0.2) -70.09%, rgba(140, 112, 253, 0) 55.52%), linear-gradient(0deg, rgba(243, 255, 228, 0.2) -26.8%, rgba(178, 215, 149, 0.012) 16.1%, rgba(220, 243, 253, 0) 131.6%), linear-gradient(239.1deg, #E2FAFF -29.57%, rgba(222, 243, 248, 0) 131.52%);
+  background: linear-gradient(
+      146.55deg,
+      rgba(174, 162, 220, 0.2) -70.09%,
+      rgba(140, 112, 253, 0) 55.52%
+    ),
+    linear-gradient(
+      0deg,
+      rgba(243, 255, 228, 0.2) -26.8%,
+      rgba(178, 215, 149, 0.012) 16.1%,
+      rgba(220, 243, 253, 0) 131.6%
+    ),
+    linear-gradient(239.1deg, #e2faff -29.57%, rgba(222, 243, 248, 0) 131.52%);
   margin-top: 1rem;
   width: 100%;
   height: 42.5rem;
@@ -66,23 +89,23 @@ export default {
   height: 16%;
   margin-bottom: 2rem;
 }
-.header-title{
-  font-family: 'Inter';
+.header-title {
+  font-family: "Inter";
   font-style: normal;
   font-weight: 600;
   font-size: 3rem;
   line-height: 97px;
   color: #172176;
 }
-.header-content{
-  font-family: 'Inter';
+.header-content {
+  font-family: "Inter";
   font-style: normal;
   font-weight: 600;
   font-size: 1rem;
   line-height: 24px;
   text-align: center;
   margin: -1%;
-  color: #47474B;
+  color: #47474b;
 }
 
 .left {
@@ -98,7 +121,7 @@ export default {
   padding-top: 1rem;
   margin: 0 auto;
 }
-.rank-list{
+.rank-list {
   padding: 0.5rem;
   width: 80%;
   height: 90%;
@@ -110,26 +133,26 @@ export default {
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
 }
-.rank-list-h3{
-  font-family: 'Exo 2';
+.rank-list-h3 {
+  font-family: "Exo 2";
   font-style: normal;
   font-weight: 700;
   font-size: 26px;
   color: #172176;
 }
-.rank-list-div{
+.rank-list-div {
   width: 90%;
   height: 4rem;
-  background: #E0F4FD;
+  background: #e0f4fd;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
   margin: 1rem;
   padding-top: 0.9rem;
 }
-.scroll{
-  overflow:scroll;
-  width:100%; 
-  height:25rem;
+.scroll {
+  overflow: scroll;
+  width: 100%;
+  height: 25rem;
 }
 .scroll::-webkit-scrollbar {
   display: none;
@@ -139,5 +162,4 @@ export default {
   -ms-overflow-style: none; /* 인터넷 익스플로러 */
   scrollbar-width: none; /* 파이어폭스 */
 }
-
 </style>
