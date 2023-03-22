0d9327f9f54112727f82341168985f4eb5b61858
박서윤 <seoyoon528@gmail.com>
2023-03-22 10:06:55 +0900

    [feat] : modify api request mapping path header '/api/v1/users' -> '/users'

--- back/vita-user/src/main/java/com/ssafy/vitauser/controller/user/UserController.java
+++ back/vita-user/src/main/java/com/ssafy/vitauser/controller/user/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
