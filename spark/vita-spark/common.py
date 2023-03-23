ef0b1d04bad324aff47211657717de5e1e84aef5
차현경 <h30young@naver.com>
2023-03-23 16:32:26 +0900

    [feat] : add common.py

---
+++ spark/vita-spark/common.py
@@ -0,0 +1,33 @@
+import boto3
+import zipfile
+import io
+
+import pymysql
+from sqlalchemy import create_engine
+
+pymysql.install_as_MySQLdb()
+import MySQLdb
+
+import config
+
+# s3 저장소 연결
+def connectS3():
+    s3 = boto3.client(service_name="s3",
+                      aws_access_key_id=config.aws_access_key_id,
+                      aws_secret_access_key=config.aws_secret_access_key)
+    return s3
+
+# s3 파일 가져온 후 zip 입축 해제
+def decompress(s3, zipName):
+    obj = s3.get_object(Bucket="vita-project-bucket", Key=zipName+".zip")
+    zipfile.ZipFile(io.BytesIO(obj["Body"].read())).extractall('./' + zipName + '/')
+
+# DB 연결
+def connectDB():
+    db_connection_str = config.db_connection_str
+    db_connection = create_engine(db_connection_str)
+    return db_connection
+
+# DB 저장
+def saveDB(db_connection, table, df):
+    df.to_sql(name=table, con=db_connection, if_exists='append', index=False)
