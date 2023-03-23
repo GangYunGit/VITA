e1db3d339751694a743a9deba89cd2a0d8a846f1
차현경 <h30young@naver.com>
2023-03-23 15:47:17 +0900

    [feat] : add calories_burned.py

---
+++ spark/vita-spark/calories_burned.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import datetime
+
+# csv 파일 읽기
+def readCsv(csvName):
+    df = pd.read_csv(csvName, header=0, 
+                     names=['active_calories_goal', 'version', 'extra_data', 'com.samsung.shealth.calories_burned.tef_calorie', 'com.samsung.shealth.calories_burned.active_time', 'com.samsung.shealth.calories_burned.rest_calorie', 'com.samsung.shealth.calories_burned.update_time', 'com.samsung.shealth.calories_burned.create_time', 'com.samsung.shealth.calories_burned.active_calorie', 'com.samsung.shealth.calories_burned.deviceuuid', 'com.samsung.shealth.calories_burned.pkg_name', 'com.samsung.shealth.calories_burned.datauuid', 'com.samsung.shealth.calories_burned.day_time', ''], 
+                     skiprows=1)
+    return df
+
+# 일 데이터 처리
+def dayDF(df):
+    df.rename(columns={'com.samsung.shealth.calories_burned.day_time':'day_time', 'com.samsung.shealth.calories_burned.active_calorie':'active_calorie'}, inplace=True)
+    df = df[['day_time', 'active_calorie']]
+    df['day_time']=df['day_time'].apply(lambda d: datetime.date.fromtimestamp((float)(d)/1000.0)) # 날짜 형식 변환
+    df.sort_values(by='day_time' ,ascending=True) # 날짜 오름차순 정렬
+    return df
