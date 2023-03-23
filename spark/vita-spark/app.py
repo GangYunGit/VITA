e10c5064e4df0f788636820dca8db1c9bb8953af
차현경 <h30young@naver.com>
2023-03-23 16:55:54 +0900

    [feat] : add zip to csv

--- spark/vita-spark/app.py
+++ spark/vita-spark/app.py
@@ -1,10 +1,22 @@
 from flask import Flask
 
+import common
+import weight
+import step_daily_trend
+import calories_burned
+import stress
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is home!'
+    return 'Hello, World!'
+
+@app.route('/upload/<userId>')
+def upload(userId):
+    s3 = common.connectS3()
+    common.decompress(s3, userId)
+    return f'Hello, {userId}!'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
