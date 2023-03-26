d46359c9417c786d9f17eefe28989866ce30b88c
김아영 <joen00@naver.com>
2023-03-26 16:56:45 +0900

    [feat] : add wearable-friend design complete

--- front/vita-front/src/views/wearable/WearableFriendView.vue
+++ front/vita-front/src/views/wearable/WearableFriendView.vue
@@ -2,20 +2,27 @@
   <div class="text-center">
     <div class="container">
       <div class="header">
-          <VueHeader
-            :VueHeaderTitle="VueHeaderTitle"
-            :VueHeaderContent="VueHeaderContent"
-          />
-        </div>
-        <div id="header-content2">
-          * 친구는 한 번에 최대 4명까지만 추가 가능합니다.
-        </div>
-
-        <div id="wfriendlist">
-          <div id="wfriendlistin">
-            <hooper :itemsToSlide="true" :itemsToShow="9" :centerMode="true">
-              <slide v-for="slide in slides" :key="slide.id" class="slide">
-                <button class="useritem" style="border: none">
+        <VueHeader
+          :VueHeaderTitle="VueHeaderTitle"
+          :VueHeaderContent="VueHeaderContent"
+        />
+      </div>
+      <div id="header-content2">
+        * 친구는 한 번에 최대 4명까지만 추가 가능합니다.
+      </div>
+      <div id="wfriendlist">
+        <!-- 여기 slide -->
+        <div id="wfriendlistin">
+          <hooper :itemsToSlide="true" :itemsToShow="9" :centerMode="true">
+            <slide v-for="slide in slides" :key="slide.id" class="slide">
+              <label class="useritem">
+                <input
+                  type="checkbox"
+                  class="useritem"
+                  :name="slide.id"
+                  :value="slide.id"
+                />
+                <span>
                   <b-avatar
                     variant="info"
                     src="https://placekitten.com/300/300"
@@ -31,13 +38,14 @@
                   >
                     {{ slide.name }}
                   </p>
-                </button>
-              </slide>
-              <hooper-navigation slot="hooper-addons"></hooper-navigation>
-              <hooper-pagination slot="hooper-addons"></hooper-pagination>
-            </hooper>
-   
-
+                </span>
+              </label>
+            </slide>
+            <hooper-navigation slot="hooper-addons"></hooper-navigation>
+            <hooper-pagination slot="hooper-addons"></hooper-pagination>
+          </hooper>
+          <!-- 여기 slide 끝 -->
+          <!-- 비교하기 버튼 -->
           <b-button
             style="
               float: right;
@@ -60,15 +68,22 @@
             친구와의 비교는 최근 1년 동안의 데이터를 기준으로 평균을 계산해
             비교해줍니다.
           </div>
+          <!-- 비교하기 버튼 -->
           <!-- 여기서 부터 걸음수 컴포넌트 등장 -->
           <div style="clear: both"></div>
-          <fwearable-walk></fwearable-walk>
-          <fwearable-energy></fwearable-energy>
+          <fwearable-walk style="margin-top: 8rem"></fwearable-walk>
+          <div style="clear: both"></div>
+          <fwearable-energy style="margin-top: 8rem"></fwearable-energy>
+          <div style="clear: both"></div>
+          <fwearable-rhr style="margin-top: 8rem"></fwearable-rhr>
+          <div style="clear: both"></div>
+          <fwearable-stress style="margin-top: 8rem"></fwearable-stress>
+          <div style="clear: both"></div>
+          <fwearable-sleep style="margin-top: 8rem"></fwearable-sleep>
         </div>
       </div>
     </div>
   </div>
-
 </template>
   
 <script>
@@ -81,8 +96,10 @@ import {
 import "hooper/dist/hooper.css";
 import FwearableWalk from "@/components/wearable_friend/FwearableWalk.vue";
 import FwearableEnergy from "@/components/wearable_friend/FwearableEnergy.vue";
+import FwearableRhr from "@/components/wearable_friend/FwearableRhr.vue";
+import FwearableStress from "@/components/wearable_friend/FwearableStress.vue";
+import FwearableSleep from "@/components/wearable_friend/FwearableSleep.vue";
 import VueHeader from "@/components/common/VueHeader.vue";
-
 
 export default {
   name: "App",
@@ -94,11 +111,17 @@ export default {
     FwearableWalk,
     FwearableEnergy,
     VueHeader,
+    FwearableRhr,
+    FwearableStress,
+    FwearableSleep,
   },
+  methods: {},
   data() {
     return {
+      selected: [], // Must be an array reference!
       VueHeaderTitle: "마이 헬스 데이터 with 프렌즈",
-      VueHeaderContent:"친구의 프로필을 클릭하고 비교하기 버튼을 누르면 친구의 건강 데이터가 실시간으로 그래프에 반영된답니다. ",
+      VueHeaderContent:
+        "친구의 프로필을 클릭하고 비교하기 버튼을 누르면 친구의 건강 데이터가 실시간으로 그래프에 반영된답니다. ",
       slides: [
         { id: 1, name: "김뿡" },
         { id: 2, name: "김뿡" },
@@ -116,7 +139,27 @@ export default {
 
 <style>
 /* ----------------------------------------- */
+/* 여기 checkbox 버튼 */
+.useritem[type="checkbox"] {
+  display: none;
+}
 
+.useritem input[type="checkbox"] + span {
+  display: inline-block;
+  text-align: center;
+  cursor: pointer;
+  width: 100px;
+  height: 100px;
+  background-color: #ffffff;
+  line-height: 100px;
+  border-radius: 20%;
+}
+
+.useritem input[type="checkbox"]:checked + span {
+  background-color: #113a6b;
+  color: #ffffff;
+}
+/* 여기 checkbox 버튼 */
 #header-content2 {
   font-family: "Inter";
   font-style: normal;
@@ -164,6 +207,9 @@ export default {
   width: 8px;
   border-radius: 50%;
   background-color: darkgrey;
+}
+.hooper-pagination {
+  bottom: -20px;
 }
 /* .hooper-slide.is-current {
   transform: scale(1.2);
