20dbcae729bec80c8c62dbbad420f299d1d79b0a
seoyoon528 <seoyoon528@gmail.com>
2023-03-27 14:01:25 +0900

    [feat] : convert Api Request 'userId' to 'AccessToken' Authorization

--- back/vita-user/src/main/java/com/ssafy/vitauser/controller/mypage/MypageController.java
+++ back/vita-user/src/main/java/com/ssafy/vitauser/controller/mypage/MypageController.java
@@ -1,8 +1,10 @@
 package com.ssafy.vitauser.controller.mypage;
 
 import com.ssafy.vitauser.dto.mypage.UserBadgeResponseDto;
+import com.ssafy.vitauser.oauth.token.AuthTokenProvider;
 import com.ssafy.vitauser.service.mypage.AwsS3Service;
 import com.ssafy.vitauser.service.mypage.MypageService;
+import com.ssafy.vitauser.util.HeaderUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -12,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -22,29 +25,41 @@ public class MypageController {
 
     private final MypageService mypageService;
     private final AwsS3Service awsS3Service;
-
+    private final AuthTokenProvider authTokenProvider;
 
     @ApiOperation(value = "유저 정보 조회", notes = "성공하면 success.", response = String.class)
-    @GetMapping("/{userId}")
-    public ResponseEntity<?> selectOne(@ApiParam(value = "user_id", required = true) @PathVariable("userId") String id){
-        return new ResponseEntity(mypageService.selectOne(id), HttpStatus.ACCEPTED);
+    @GetMapping
+    public ResponseEntity<?> selectOne(HttpServletRequest request){
+        String accessToken = HeaderUtil.getAccessToken(request);
+        String userId = authTokenProvider.getUserId(accessToken);
+
+        return new ResponseEntity(mypageService.selectOne(userId), HttpStatus.ACCEPTED);
     }
 
     @ApiOperation(value = "유저 뱃지 정보 조회", notes = "성공하면 success.", response = String.class)
-    @GetMapping("/badge/{userId}")
-    public ResponseEntity<?> selectAllBadge(@ApiParam(value = "user_id", required = true) @PathVariable("userId") String id){
-        return new ResponseEntity(mypageService.selectAllBadge(id), HttpStatus.ACCEPTED);
+    @GetMapping("/badge")
+    public ResponseEntity<?> selectAllBadge(HttpServletRequest request){
+        String accessToken = HeaderUtil.getAccessToken(request);
+        String userId = authTokenProvider.getUserId(accessToken);
+
+        return new ResponseEntity(mypageService.selectAllBadge(userId), HttpStatus.ACCEPTED);
     }
 
     @ApiOperation(value = "유저 히스토리 정보 조회", notes = "성공하면 success.", response = String.class)
-    @GetMapping("/history/{userId}")
-    public ResponseEntity<?> selectAllHistory(@ApiParam(value = "user_id", required = true) @PathVariable("userId") String id){
-        return new ResponseEntity(mypageService.selectAllHistory(id), HttpStatus.ACCEPTED);
+    @GetMapping("/history")
+    public ResponseEntity<?> selectAllHistory(HttpServletRequest request){
+        String accessToken = HeaderUtil.getAccessToken(request);
+        String userId = authTokenProvider.getUserId(accessToken);
+
+        return new ResponseEntity(mypageService.selectAllHistory(userId), HttpStatus.ACCEPTED);
     }
 
     @ApiOperation(value = "유저 파일 업로드 저장", notes = "성공하면 success.", response = String.class)
-    @PostMapping("/upload/{userId}")
-    public ResponseEntity<String> uploadFile(@RequestParam("multipartFile") MultipartFile multipartFile, @PathVariable String userId) throws Exception {
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("multipartFile") MultipartFile multipartFile, HttpServletRequest request) throws Exception {
+        String accessToken = HeaderUtil.getAccessToken(request);
+        String userId = authTokenProvider.getUserId(accessToken);
+
         // 파일 저장함
         String url = awsS3Service.uploadFileV1(multipartFile);
         // url update 해줌
