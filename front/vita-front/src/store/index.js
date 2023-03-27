ef8257dbf462d12bad4eb06ff28384d48877fd27
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2023-03-28 01:47:10 +0900

    [feat] : fix port

--- front/vita-front/src/store/index.js
+++ front/vita-front/src/store/index.js
@@ -10,8 +10,8 @@ export default new Vuex.Store({
   state: {
     test_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNzAzNjI5NjE0IiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.dV7CsWFo0_-ICBkvTlIQP-75qAuQg_hJ_sJcFjzjLx0',
     // 배포서버
-    url: 'https://j8b106.p.ssafy.io:8000/wearable/user/',
-    friendUrl: 'https://j8b106.p.ssafy.io:8000/wearable/friend',
+    url: 'https://j8b106.p.ssafy.io/api/wearable/user/',
+    friendUrl: 'https://j8b106.p.ssafy.io/api/wearable/friend',
     // 로컬서버
     // url: 'http://localhost:8083/wearable/user/',
     // friendUrl: 'http://localhost:8083/wearable/friend',
@@ -19,7 +19,7 @@ export default new Vuex.Store({
     myUserId: "2715879100",
     // ec2서버 : "https://j8b106.p.ssafy.io:8000"
     // 로컬 유저 서버: "http://localhost:8085"
-    serverBaseUrl: "https://j8b106.p.ssafy.io:8000"
+    serverBaseUrl: "https://j8b106.p.ssafy.io/api"
   },
   getters: {
   },
