6e645012b877322f73d50d5fce60c66d12f229fa
김아영 <joen00@naver.com>
2023-03-27 13:40:19 +0900

    [feat] : add apigateway

--- back/vita-wearable/src/main/java/com/ssafy/vitawearable/repo/ApiAverageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.vitawearable.repo;
-
-import com.ssafy.vitawearable.entity.ApiAverage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApiAverageRepo extends JpaRepository<ApiAverage,Long> {
-}
