b6f8a7d3e0c9920e868d44ce408bc6297dbf0b00
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-03-28 14:41:44 +0900

    [feat] : update friend rank API

--- back/vita-friend/src/main/java/com/ssafy/vitafriend/repository/FriendRepository.java
+++ back/vita-friend/src/main/java/com/ssafy/vitafriend/repository/FriendRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
-    // 친구 검색 창에서 닉네임 입력시
+    // 친구 검색 창에서 닉네임으로 조회(Like 조건)
     @Query(value = "SELECT DISTINCT u.user_id, u.user_nickname, u.user_img, f.friend_status FROM user u " +
             "LEFT OUTER JOIN friend f ON (u.user_id = f.friend_sending_user_id OR u.user_id = f.friend_receiving_user_id) " +
             "AND (f.friend_sending_user_id = ?1 OR f.friend_receiving_user_id = ?1) AND f.friend_status = 'accepted' " +
@@ -19,6 +19,7 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     )
     List<FriendSearchMapping> getFriendSearchListLike(String userId, String userNickname);
 
+    // 모든 친구 조회
     @Query(value = "SELECT DISTINCT u.user_id, u.user_nickname, u.user_img, f.friend_status FROM user u " +
             "LEFT OUTER JOIN friend f ON (u.user_id = f.friend_sending_user_id OR u.user_id = f.friend_receiving_user_id) " +
             "AND (f.friend_sending_user_id = ?1 OR f.friend_receiving_user_id = ?1) AND f.friend_status = 'accepted' "
@@ -26,13 +27,17 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     )
     List<FriendSearchMapping> getFriendSearchList(String userId);
 
+    // 친구 요청, 친구 수락 저장
     Friend save(Friend friend);
+
+    // 친구 신청을 보내는 사람, 받는 사람의 상태(accepted 혹은 applied) 조회
     List<FriendSendingListDto> findByFriendSendingUser_userIdAndFriendStatus(String sendingUserId, String friendStatus);
     List<FriendReceivingListDto> findByFriendReceivingUser_userIdAndFriendStatus(String receivingUserId, String friendStatus);
 
+    // 친구 신청을 보내는 사람, 받는 사람의 상태(accepted 혹은 applied) 조회 (Like 조건 추가)
     List<FriendSendingListDto> findByFriendSendingUser_userIdAndFriendStatusAndFriendReceivingUser_userNicknameContains(String sendingUserId, String friendStatus, String nickname);
     List<FriendReceivingListDto> findByFriendReceivingUser_userIdAndFriendStatusAndFriendSendingUser_userNicknameContains(String receivingUserId, String friendStatus, String nickname);
-
+    
     Friend findByFriendSendingUser_userIdAndFriendReceivingUser_userIdAndFriendStatus(String sendingUserId, String receivingUserId, String friendStatus);
     Friend findByFriendSendingUser_userIdAndFriendReceivingUser_userId(String sendingUserId, String receivingUserId);
 
