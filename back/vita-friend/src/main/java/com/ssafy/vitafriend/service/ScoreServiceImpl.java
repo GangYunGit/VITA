434fd2e348ea9c84fed559359dff3564ff8c60bb
이강윤 <lky61010@naver.com>
2023-03-29 22:52:45 +0900

    [feat] : update rank API for no-exported user

--- back/vita-friend/src/main/java/com/ssafy/vitafriend/service/ScoreServiceImpl.java
+++ back/vita-friend/src/main/java/com/ssafy/vitafriend/service/ScoreServiceImpl.java
@@ -21,13 +21,28 @@ public class ScoreServiceImpl implements ScoreService{
     @Override
     public FriendTotalRankDto getTotalScoreByUser(String userId) {
         Score getUserScoreInfo = scoreRepository.findFirstByUser_userIdOrderByCreatedDateDesc(userId);
-        FriendTotalRankDto userScoreInfo = modelMapper.map(getUserScoreInfo, FriendTotalRankDto.class);
-        int totalScore = userScoreInfo.getTotalScoreEnergy() +
-                userScoreInfo.getTotalScoreSleep() + userScoreInfo.getTotalScoreRhr() +
-                userScoreInfo.getTotalScoreStep() + userScoreInfo.getTotalScoreStress();
-        userScoreInfo.setTotalScore(totalScore/5);
+        System.out.println(getUserScoreInfo);
+        // 친구 추가된 유저의 점수 정보가 있다면 계산하여 표시
+        if(getUserScoreInfo != null) {
+            FriendTotalRankDto userScoreInfo = modelMapper.map(getUserScoreInfo, FriendTotalRankDto.class);
+            int totalScore = userScoreInfo.getTotalScoreEnergy() +
+                    userScoreInfo.getTotalScoreSleep() + userScoreInfo.getTotalScoreRhr() +
+                    userScoreInfo.getTotalScoreStep() + userScoreInfo.getTotalScoreStress();
+            userScoreInfo.setTotalScore(totalScore/5);
 
-        return userScoreInfo;
+            return userScoreInfo;
+        }
+        // 친구 추가된 유저의 점수 정보가 없다면 기본값이 모두 0으로 표시됨
+        else {
+            FriendTotalRankDto noUserScoreInfo = new FriendTotalRankDto();
+            noUserScoreInfo.setTotalScore(0);
+            noUserScoreInfo.setTotalScoreStep(0);
+            noUserScoreInfo.setTotalScoreEnergy(0);
+            noUserScoreInfo.setTotalScoreStress(0);
+            noUserScoreInfo.setTotalScoreRhr(0);
+            noUserScoreInfo.setTotalScoreSleep(0);
+            return noUserScoreInfo;
+        }
     }
 
 }
