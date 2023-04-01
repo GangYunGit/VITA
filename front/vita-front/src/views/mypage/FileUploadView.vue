994b25a3eb1b79db2af09fbad4072e8542a4efd8
김아영 <joen00@naver.com>
2023-04-01 21:58:40 +0900

    [feat] : add upload

--- front/vita-front/src/views/mypage/FileUploadView.vue
+++ front/vita-front/src/views/mypage/FileUploadView.vue
@@ -2,39 +2,45 @@
   <div class="text-center">
     <div class="container">
       <div id="header">
-        <!-- <div id="header-title">파일 업로드</div>
-        <div id="header-content">웨어러블기능을 이용하기 위해 수집된 데이터를 업로드 해보세요.</div> -->
         <VueHeader
           :VueHeaderTitle="VueHeaderTitle"
           :VueHeaderContent="VueHeaderContent"
         />
       </div>
+      <!-- 설명란 -->
       <div id="fileuploadLeft">
         <div>
-          <b-carousel
-            id="carousel-1"
-            v-model="slide"
-            controls
-            indicators
-            img-width="1000"
-            img-height="400"
-            style="text-shadow: 1px 1px 2px #333"
-            @sliding-start="onSlideStart"
-            @sliding-end="onSlideEnd"
-          >
-            <!-- Slides with image only -->
-            <b-carousel-slide
-              img-src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFVrAD%2Fbtr45lx4C4m%2FLKmrLEn0bWNwj0f3q9qDD1%2Fimg.png"
-            ></b-carousel-slide>
-            <b-carousel-slide
-              img-src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FsW7IR%2Fbtr42z4Ewb9%2F8jZo4oAZ8BZQEDp5SqFfhk%2Fimg.png"
-            ></b-carousel-slide>
-            <b-carousel-slide
-              img-src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FccVdIO%2Fbtr41J0KddD%2Fir81Nks5a9nLkqLlkCYTKk%2Fimg.png"
-            ></b-carousel-slide>
-          </b-carousel>
+          <hooper style="height: 50%">
+            <slide>
+              <div>
+                <img
+                  width="80%"
+                  src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FccVdIO%2Fbtr41J0KddD%2Fir81Nks5a9nLkqLlkCYTKk%2Fimg.png"
+                />
+              </div>
+            </slide>
+            <slide>
+              <div>
+                <img
+                  width="80%"
+                  src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFVrAD%2Fbtr45lx4C4m%2FLKmrLEn0bWNwj0f3q9qDD1%2Fimg.png"
+                />
+              </div>
+            </slide>
+            <slide>
+              <div>
+                <img
+                  width="80%"
+                  src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FsW7IR%2Fbtr42z4Ewb9%2F8jZo4oAZ8BZQEDp5SqFfhk%2Fimg.png"
+                />
+              </div>
+            </slide>
+
+            <hooper-navigation slot="hooper-addons"></hooper-navigation>
+          </hooper>
         </div>
       </div>
+      <!-- 파일 업로드 -->
       <div id="fileuploadRight">
         <b-form-file
           v-model="file1"
@@ -43,7 +49,8 @@
           drop-placeholder="Drop file here..."
         ></b-form-file>
         <div class="mt-3">Selected file: {{ file1 }}</div>
-        <b-button @click="uploadFile(file1)">Button</b-button>
+        <!-- 파일 업로드 버튼 -->
+        <b-button @click="uploadFile(file1)">파일 업로드</b-button>
       </div>
     </div>
   </div>
@@ -53,22 +60,37 @@
 import VueHeader from "@/components/common/VueHeader.vue";
 import axios from "axios";
 import { mapGetters } from "vuex";
+import { Hooper, Slide, Navigation as HooperNavigation } from "hooper";
+import "hooper/dist/hooper.css";
 
 export default {
   name: "FileUploadView",
   components: {
     VueHeader,
+    Hooper,
+    Slide,
+    Hooper,
+    Slide,
+    HooperNavigation,
   },
   data: () => ({
     VueHeaderTitle: "파일 업로드",
     VueHeaderContent:
       "웨어러블기능을 이용하기 위해 수집된 데이터를 업로드 해보세요.",
     file1: null,
+    slide: 0,
+    sliding: null,
   }),
   computed: {
     ...mapGetters(["token", "user"]),
   },
   methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
     uploadFile(file) {
       if (file == null) {
         alert("파일을 먼저 업로드 하세요!");
@@ -91,13 +113,15 @@ export default {
         )
         .then((response) => {
           console.log(response);
-          const userId = response.data 
+          const userId = response.data;
           axios
-            .get(`https://j8b106.p.ssafy.io:5000/upload`, { params: { userId: userId } })
+            .get(`https://j8b106.p.ssafy.io:5000/upload`, {
+              params: { userId: userId },
+            })
             .then((response) => {
-              console.log(response)
+              console.log(response);
             });
-            });
+        });
     },
     created() {
       console.log(this.serverBaseUrl);
@@ -110,13 +134,13 @@ export default {
 /* ----------------------------------------- */
 #fileuploadLeft {
   width: 65%;
-  height: 42rem;
+
   float: left;
   margin-top: 1rem;
 }
 #fileuploadRight {
   width: 35%;
-  height: 42rem;
+
   float: left;
 }
 </style>
