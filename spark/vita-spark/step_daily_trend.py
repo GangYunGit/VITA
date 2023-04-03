cde56846209b63acf489681a8a7e7ce3c7c85639
차현경 <h30young@naver.com>
2023-04-03 10:03:31 +0900

    [feat] : add apple watch step daily trend

--- spark/vita-spark/step_daily_trend.py
+++ spark/vita-spark/step_daily_trend.py
@@ -15,3 +15,11 @@ def samsung_day(df):
     df = df[['daily_wearable_step', 'date']]
     df = df.groupby('date', as_index=False).max() # 날짜별 최대값
     return df
+
+# Apple 일별 데이터 처리
+def apple_day(data):
+    df = pd.DataFrame(data, columns=['date', 'daily_wearable_step'])
+    df['date'] = df.date.str.split(' ').str[0] # 날짜 형식
+    df['daily_wearable_step'] = df['daily_wearable_step'].astype(float)
+    df = df.groupby('date', as_index=False).sum() # 날짜별 합계
+    return df
